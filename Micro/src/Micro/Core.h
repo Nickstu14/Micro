d62ca71f33a749d169f0887b8a2d39a2b03f1ada
Nickstu14 <nickstu14@hotmail.co.uk>
2020-02-06 17:54:06 +0000

    Modified some originaly missed settings

--- Micro/src/Micro/Core.h
+++ Micro/src/Micro/Core.h
@@ -2,9 +2,9 @@
 
 #ifdef MC_PLATFORM_WINDOWS
 	#ifdef MC_BUILD_DLL
-		#define MICRO_API	_declspec(dllexport)
+		#define MICRO_API	__declspec(dllexport)
 	#else
-		#define MICRO_API	_declspec(dllimport)
+		#define MICRO_API	__declspec(dllimport)
 	#endif
 #else
 	#error Micro only supports windows!
